9ac696b0eb663f34b5dbf9183d7aa04a969f3654
kuvasney <kuvasney@gmail.com>
2019-11-08 01:11:07 -0300

    paginacao e css

--- filtro.js
+++ filtro.js
@@ -4,7 +4,7 @@ const itens = [
         image: 'https://picsum.photos/200',
         category: 'Webinar 1',
         type: 'Lorem Ipsumt',
-        maintitle: 'Incluídos em sua associação',
+        maintitle: '1 Incluídos em sua associação',
         text: 'Pleasure to use so that so-called “iPad killer”, in conclusion Android is fragmented, but while Jony Ive’s incredible design, for this reason Steve Jobs was a genius.',
         link: 'http://foo.cm'
     },
@@ -12,7 +12,7 @@ const itens = [
         image: 'https://picsum.photos/200',
         category: 'Outra Categoria 2',
         type: 'Trollem ipsum',
-        maintitle: 'Having trouble finding the right words? Let trollem ipsum help you out!',
+        maintitle: '2 Having trouble finding the right words? Let trollem ipsum help you out!',
         text: 'CrackBerry, immediately delay in getting Ice Cream Sandwich I would say that Flash sucks, moreover awful user experience, when profit owing to Apple will only get better.',
         link: 'http://foo2.cm'
     },
@@ -20,7 +20,7 @@ const itens = [
         image: 'https://picsum.photos/200',
         category: 'Links 3',
         type: 'Lorem Ipsumt',
-        maintitle: 'Android geek why Flash sucks eventually so-called “iPad killer”',
+        maintitle: '3 Android geek why Flash sucks eventually so-called “iPad killer”',
         text: 'Gorgeous, as well as battery life but also MacBook Air is just beautiful however Steve Jobs was a genius, especially awful user experience overall genius, owing to Android is fragmented.',
         link: 'http://lipsum.com'
     },
@@ -28,7 +28,71 @@ const itens = [
         image: 'https://picsum.photos/200',
         category: 'Webinar 1',
         type: 'Trollem ipsum',
-        maintitle: 'Android geek why Flash sucks',
+        maintitle: '4 Android geek why Flash sucks',
+        text: 'Gorgeous, as well as battery life but also MacBook Air is just beautiful however Steve Jobs was a genius, especially awful user experience overall genius, owing to Android is fragmented.',
+        link: 'http://lipsum.com'
+    },
+    {
+        image: 'https://picsum.photos/200',
+        category: 'Webinar 1',
+        type: 'Lorem Ipsumt',
+        maintitle: '5 Incluídos em sua associação',
+        text: 'Pleasure to use so that so-called “iPad killer”, in conclusion Android is fragmented, but while Jony Ive’s incredible design, for this reason Steve Jobs was a genius.',
+        link: 'http://foo.cm'
+    },
+    {
+        image: 'https://picsum.photos/200',
+        category: 'Outra Categoria 2',
+        type: 'Trollem ipsum',
+        maintitle: '6 Having trouble finding the right words? Let trollem ipsum help you out!',
+        text: 'CrackBerry, immediately delay in getting Ice Cream Sandwich I would say that Flash sucks, moreover awful user experience, when profit owing to Apple will only get better.',
+        link: 'http://foo2.cm'
+    },
+    {
+        image: 'https://picsum.photos/200',
+        category: 'Links 3',
+        type: 'Lorem Ipsumt',
+        maintitle: '7 Android geek why Flash sucks eventually so-called “iPad killer”',
+        text: 'Gorgeous, as well as battery life but also MacBook Air is just beautiful however Steve Jobs was a genius, especially awful user experience overall genius, owing to Android is fragmented.',
+        link: 'http://lipsum.com'
+    },
+    {
+        image: 'https://picsum.photos/200',
+        category: 'Webinar 1',
+        type: 'Trollem ipsum',
+        maintitle: '8 Android geek why Flash sucks',
+        text: 'Gorgeous, as well as battery life but also MacBook Air is just beautiful however Steve Jobs was a genius, especially awful user experience overall genius, owing to Android is fragmented.',
+        link: 'http://lipsum.com'
+    },
+    {
+        image: 'https://picsum.photos/200',
+        category: 'Webinar 1',
+        type: 'Lorem Ipsumt',
+        maintitle: '9 Incluídos em sua associação',
+        text: 'Pleasure to use so that so-called “iPad killer”, in conclusion Android is fragmented, but while Jony Ive’s incredible design, for this reason Steve Jobs was a genius.',
+        link: 'http://foo.cm'
+    },
+    {
+        image: 'https://picsum.photos/200',
+        category: 'Outra Categoria 2',
+        type: 'Trollem ipsum',
+        maintitle: '10 Having trouble finding the right words? Let trollem ipsum help you out!',
+        text: 'CrackBerry, immediately delay in getting Ice Cream Sandwich I would say that Flash sucks, moreover awful user experience, when profit owing to Apple will only get better.',
+        link: 'http://foo2.cm'
+    },
+    {
+        image: 'https://picsum.photos/200',
+        category: 'Links 3',
+        type: 'Lorem Ipsumt',
+        maintitle: '11 Android geek why Flash sucks eventually so-called “iPad killer”',
+        text: 'Gorgeous, as well as battery life but also MacBook Air is just beautiful however Steve Jobs was a genius, especially awful user experience overall genius, owing to Android is fragmented.',
+        link: 'http://lipsum.com'
+    },
+    {
+        image: 'https://picsum.photos/200',
+        category: 'Webinar 1',
+        type: 'Trollem ipsum',
+        maintitle: '12 Android geek why Flash sucks',
         text: 'Gorgeous, as well as battery life but also MacBook Air is just beautiful however Steve Jobs was a genius, especially awful user experience overall genius, owing to Android is fragmented.',
         link: 'http://lipsum.com'
     },
@@ -36,7 +100,7 @@ const itens = [
         image: 'https://picsum.photos/200',
         category: 'Outra Categoria 2',
         type: 'Trollem ipsum',
-        maintitle: 'Let trollem ipsum help you out!',
+        maintitle: '13 Let trollem ipsum help you out!',
         text: 'CrackBerry, immediately delay in getting Ice Cream Sandwich.',
         link: 'http://foo2.cm'
     }, 
@@ -44,7 +108,7 @@ const itens = [
         image: 'https://picsum.photos/200',
         category: 'Webinar 1',
         type: 'Trollem ipsum',
-        maintitle: 'Afrima is playing',
+        maintitle: '14 Afrima is playing',
         text: 'And it\'s gonna take some time.',
         link: 'http://lipsum.com'
     },
@@ -52,7 +116,11 @@ const itens = [
 
 // SETUP DOS ITENS
 const itensSpecs = {
-    width: 20
+    width: 25,
+    start: 0,
+    end: 6,
+    limit: 6,
+    page: 1
 }
 
 function filter() {
@@ -64,7 +132,7 @@ function filter() {
     let filteredItens = [];
     let categoriesFilter = document.querySelector('#category').value;
     let typesFilter = document.querySelector('#type').value;
-    
+        
     // SE O FILTRO DE CATEGORIA TROUXER ALGUMA CATEGORIA, DEVO FILTRAR
     if (categoriesFilter !== '0') {
         filteredItens = itens.filter(function(item){
@@ -84,29 +152,54 @@ function filter() {
 
     // PERCORRO OS ITENS JÁ FILTRADOS
     if (filteredItens.length > 0) {
-        filteredItens.forEach(function(item) {
+        for (let i = itensSpecs.start; i < itensSpecs.end && i < filteredItens.length; i++) {
             document.querySelector('.filter__itens').innerHTML += `<li style="width:${itensSpecs.width}vw">
-                <div class="img>
-                    <a href="${item.link}">
-                        <img src="${item.image}" alt="${item.maintitle}">
+                <div class="img">
+                    <a href="${filteredItens[i].link}">
+                        <img src="${filteredItens[i].image}" alt="${filteredItens[i].maintitle}">
                     </a>
+                    <span class="category">
+                        ${filteredItens[i].category}
+                    </span>
                 </div>
                 <div class="content">
-                    <span class="category">
-                        ${item.category}
-                    </span>
                     <p class="type">
-                        ${item.type}
+                        ${filteredItens[i].type}
                     </p>
-                    <a href="${item.link}">
-                        <h3>${item.maintitle}</h3>
-                    </a>
-                    <a href="${item.link}">
-                        <p>${item.text}</p>
-                    </a>
+                    <h3 class="maintitle">
+                        <a href="${filteredItens[i].link}">
+                            ${filteredItens[i].maintitle}
+                        </a>
+                    </h3>
                 </div>
-            </li>`;            
-        })        
+            </li>`; 
+
+        }
+        
+
+        // filteredItens.forEach(function(item) {
+        //     document.querySelector('.filter__itens').innerHTML += `<li style="width:${itensSpecs.width}vw">
+        //         <div class="img>
+        //             <a href="${item.link}">
+        //                 <img src="${item.image}" alt="${item.maintitle}">
+        //             </a>
+        //         </div>
+        //         <div class="content">
+        //             <span class="category">
+        //                 ${item.category}
+        //             </span>
+        //             <p class="type">
+        //                 ${item.type}
+        //             </p>
+        //             <a href="${item.link}">
+        //                 <h3>${item.maintitle}</h3>
+        //             </a>
+        //             <a href="${item.link}">
+        //                 <p>${item.text}</p>
+        //             </a>
+        //         </div>
+        //     </li>`;            
+        // })        
     } else {
         document.querySelector('.filter__itens').innerHTML = `<li>Sua busca não retornou resultados</li>`
     }
@@ -136,6 +229,38 @@ function filter() {
     // PRECISO LIMPAR E RECRIAR AS OPCOES DO SELECT COM A OPCAO '0' INCLUSA. É UMA GAMBIARRA FANTASTICA, ADMITA
     document.querySelector('#category').innerHTML = `<option value="0">Selecione</option> ${categoriesOptions}`
     document.querySelector('#type').innerHTML = `<option value="0">Selecione</option> ${typesOption}`
+    
+    
+    pagination(filteredItens.length)
 }
 
+
+function pagination (total) {
+    console.log(total)
+    
+    // // CRIO A PAGINACAO, CASO NECESSARIA
+    if (total > itensSpecs.limit) {
+        document.querySelector('.pagination').innerHTML = '';
+        let pageNumbers = '';
+        pageNumbers = Math.ceil(total / itensSpecs.limit);
+        for (let j = 1; j <= pageNumbers; j++) {
+            document.querySelector('.pagination').innerHTML += `<li class="${j == itensSpecs.page ? 'on' : ''}">${j}</li>`
+        }
+        create();
+    }
+
+    function create() {
+        let paginationItens = document.querySelectorAll('.pagination li');
+        paginationItens.forEach(function(p) {
+            p.addEventListener('click', function(){
+                itensSpecs.page = parseInt(p.textContent);
+                itensSpecs.end = parseInt(p.textContent) * itensSpecs.limit;
+                itensSpecs.start = itensSpecs.end - itensSpecs.limit;
+                filter();
+            }) 
+        })
+    }
+
+    
+}
 filter();
